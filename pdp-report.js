039626af3d7dc5d4e21e190deeecb7a4dfd0f861
Dave Stanton <davestanton.us@gmail.com>
2020-07-10 17:25:00 -0400

    Creating a PDF for each record but overwrites.

--- pdp-report.js
+++ pdp-report.js
@@ -1,6 +1,8 @@
 const os = require('os');
+const fspdf = require('fs');
 const fs = require('fs').promises;
 const parse = require('csv-parse/lib/sync');
+const PDFDocument = require('pdfkit');
 
 (async function(){
   // Read the content
@@ -11,11 +13,36 @@ const parse = require('csv-parse/lib/sync');
     skip_empty_lines: true
   })
   // Iterate through the records array
-  records.forEach(element => {
-    for (const [key, value] of Object.entries(element)) {
+  records.forEach(record => {
+    
+    // Create a document and pipe output
+    const doc = new PDFDocument;
+    doc.pipe(fspdf.createWriteStream(`output/pdp-report.pdf`));
+
+    // Example output
+    doc.fontSize(25)
+        .text('foo', 100, 100)
+        .text('bar', 100, 200)
+        .text('baz', 100, 300);
+        
+    doc.addPage();
+    doc.moveTo(100, 100)
+        .lineTo(400, 100)
+        .lineTo(400, 200)
+        .lineTo(100, 200)
+        .fill("#FF3300");
+
+    // Real keys
+    for (const [key, value] of Object.entries(record)) {
       console.log(`${key}: ${value}`);
+      doc.addPage()
+          .fontSize(25)
+          .text(`${key}: ${value}`, 100, 100);
     }
-    console.log('-----------------')
+
+    doc.end();
+    console.log("\n\n");
+
   });
 
 })()
