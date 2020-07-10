2a3086485990030722384a956c9b0abae59c6248
Dave Stanton <davestanton.us@gmail.com>
2020-07-10 17:43:22 -0400

    Multiple reports with timestamp.

--- pdp-report.js
+++ pdp-report.js
@@ -3,6 +3,7 @@ const fspdf = require('fs');
 const fs = require('fs').promises;
 const parse = require('csv-parse/lib/sync');
 const PDFDocument = require('pdfkit');
+const now = Date.now();
 
 (async function(){
   // Read the content
@@ -15,21 +16,21 @@ const PDFDocument = require('pdfkit');
   // Iterate through the records array
   records.forEach(record => {
     
+    const reportId = record['ReportId']
+
     // Create a document and pipe output
     const doc = new PDFDocument;
-    doc.pipe(fspdf.createWriteStream(`output/pdp-report.pdf`));
+    doc.pipe(fspdf.createWriteStream(`output/pdp-report-${reportId}.pdf`));
 
     // Example output
     doc.fontSize(25)
-        .text('foo', 100, 100)
-        .text('bar', 100, 200)
-        .text('baz', 100, 300);
-        
-    doc.addPage();
-    doc.moveTo(100, 100)
-        .lineTo(400, 100)
-        .lineTo(400, 200)
-        .lineTo(100, 200)
+        .text('Plastics Disclosure Project', 100, 100)
+        .text('Report Format v0.0.1', 100, 200)
+        .text(`Generated at ${now}`, 100, 300);
+    doc.moveTo(100, 400)
+        .lineTo(400, 400)
+        .lineTo(400, 500)
+        .lineTo(100, 500)
         .fill("#FF3300");
 
     // Real keys
